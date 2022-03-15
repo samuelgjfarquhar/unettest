604818bcb2886d56470b4d405cc0b2f4c6163348
samuelgjfarquhar <40397363@live.napier.ac.uk>
2022-03-15 00:21:08 +0000

    WORKING-LIVER-UNET

--- data_handler.py
+++ data_handler.py
@@ -6,12 +6,11 @@ from sklearn.preprocessing import MinMaxScaler
 import random
 import matplotlib.pyplot as plt
 import torchio as tio
-
+import skimage.transform as skTrans
 scaler = MinMaxScaler()
 
-liver_list = sorted(glob.glob('D:\\liver\\test\\train\\*.nii'))
-
-mask_list = sorted(glob.glob('D:\\liver\\test\\mask\\*.nii'))
+liver_list = sorted(glob.glob('D:\\liver\\train\\*.nii'))
+mask_list = sorted(glob.glob('D:\\liver\\mask\\*.nii'))
 
 #Training numpy conversion
 for img in range(len(liver_list)):   #Using t1_list as all lists are of same size
@@ -21,23 +20,26 @@ for img in range(len(liver_list)):   #Using t1_list as all lists are of same siz
     val_liver=scaler.fit_transform(val_liver.reshape(-1, val_liver.shape[-1])).reshape(val_liver.shape)
            
     val_mask=nib.load(mask_list[img]).get_fdata()
-    val_mask=val_mask.astype(np.uint8)
-        
+    val_mask=scaler.fit_transform(val_mask.reshape(-1, val_mask.shape[-1])).reshape(val_mask.shape)
+    
+    
     combined_images =val_liver[177:443, 135:391, 340:596]
     combined_mask = val_mask[177:443, 135:391, 340:596]
     
-    import skimage.transform as skTrans
     combined_images = skTrans.resize(combined_images, (128,128,128), order=1, preserve_range=True)
     combined_mask = skTrans.resize(combined_mask, (128,128,128), order=1, preserve_range=True)  
         
-    np.save('D:\\liver\\test\\npyTrain\\liver_'+str(img)+'.npy', combined_images)
-    np.save('D:\\liver\\test\\npyMask\\liver_'+str(img)+'.npy', combined_mask)
+    temp_liver = np.stack([combined_images], axis=3)
+    temp_mask= to_categorical(combined_mask, num_classes=3)
+    
+    np.save('D:\\test\\npyTrain\\liver_'+str(img)+'.npy', temp_liver)
+    np.save('D:\\test\\npyMask\\liver_'+str(img)+'.npy', temp_mask)
 
     print(f'img {img} converted')
 
 
-img = np.load('D:\\liver\\test\\npyTrainTest\\liver_1.npy')
-mask = np.load('D:\\liver\\test\\npyMaskTest\\liver_1.npy')
+img = np.load('D:\\test\\npyTrainTest\\liver_1.npy')
+mask = np.load('D:\\test\\npyMaskTest\\liver_1.npy')
 
 plt.subplot(234)
 plt.imshow(img[:,:,50], cmap='gray')
@@ -57,4 +59,27 @@ plt.subplot(235)
 plt.imshow(testmask[:,:,100], cmap='gray')
 plt.title('Mask')
 plt.show()
+
+#################################
+
+val_liver=nib.load(liver_list[1]).get_fdata()
+val_liver=scaler.fit_transform(val_liver.reshape(-1, val_liver.shape[-1])).reshape(val_liver.shape)
+           
+val_mask=nib.load(mask_list[1]).get_fdata()
+val_mask=scaler.fit_transform(val_mask.reshape(-1, val_mask.shape[-1])).reshape(val_mask.shape)
+    
+    
+combined_images =val_liver[177:443, 135:391, 340:596]
+combined_mask = val_mask[177:443, 135:391, 340:596]
+    
+    
+import skimage.transform as skTrans
+combined_images = skTrans.resize(combined_images, (128,128,128), order=1, preserve_range=True)
+combined_mask = skTrans.resize(combined_mask, (128,128,128), order=1, preserve_range=True)  
+    
+temp_liver = np.stack([combined_images], axis=3)
+temp_mask= to_categorical(combined_mask, num_classes=3)
+    
+np.save('D:\\liver\\test\\npyTrainTest\\liver_1.npy', temp_liver)
+np.save('D:\\liver\\test\\npyMaskTest\\liver_1.npy', temp_mask)
 
--- train3.py
+++ train3.py
@@ -15,15 +15,16 @@ import random
 from optparse import OptionParser
 import tensorflow_addons as tfa
 
+tf.config.run_functions_eagerly=True
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 os.chdir('C:/Users/samue/BraTS2020/')
 
 ####################################################
-train_img_dir = "D:/liver/test/npyTrainTest/"
-train_mask_dir = "D:/liver/test/npyMaskTest/"
+train_img_dir = "D:/test/npyTrain/"
+train_mask_dir = "D:/test/npyMask/"
 
-val_img_dir = "D:/liver/test/npyValTest/"
-val_mask_dir = "D:/liver/test/npyValMaskTest/"
+val_img_dir = "D:/test/npyVal/"
+val_mask_dir = "D:/test/npyValMask/"
 
 train_img_list = os.listdir(train_img_dir)
 train_mask_list = os.listdir(train_mask_dir)
@@ -55,9 +56,9 @@ plt.imshow(test_mask[:,:,n_slice])
 plt.title('Mask')
 plt.show()
 
-wt0, wt1, wt2 = 0.26, 22.53, 22.53
+wt0, wt1, wt2, wt3 = 0.26, 22.53, 22.53, 26.21
 
-dice_loss = sm.losses.DiceLoss(class_weights=np.array([wt0, wt1, wt2]))
+dice_loss = sm.losses.DiceLoss(class_weights=np.array([wt0, wt1, wt2, wt3]))
 focal_loss = sm.losses.CategoricalFocalLoss()
 total_loss = dice_loss + (1 * focal_loss)
 
@@ -75,7 +76,7 @@ model = unet_model(IMG_HEIGHT=128,
                           IMG_CHANNELS=1,
                           num_classes=3)
 
-model.compile(optimizer=optim, loss=total_loss, metrics=metrics)
+model.compile(optimizer=optim, loss=focal_loss, metrics=metrics)
 print(model.summary())
 
 print(model.input_shape)
@@ -93,125 +94,125 @@ model.save('brats_3d.hdf5')
 ##################################################################
 
 
-# plot the training and validation IoU and loss at each epoch
-loss = history.history['loss']
-val_loss = history.history['val_loss']
-epochs = range(1, len(loss) + 1)
-plt.plot(epochs, loss, 'y', label='Training loss')
-plt.plot(epochs, val_loss, 'r', label='Validation loss')
-plt.title('Training and validation loss')
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.legend()
-plt.show()
+# # plot the training and validation IoU and loss at each epoch
+# loss = history.history['loss']
+# val_loss = history.history['val_loss']
+# epochs = range(1, len(loss) + 1)
+# plt.plot(epochs, loss, 'y', label='Training loss')
+# plt.plot(epochs, val_loss, 'r', label='Validation loss')
+# plt.title('Training and validation loss')
+# plt.xlabel('Epochs')
+# plt.ylabel('Loss')
+# plt.legend()
+# plt.show()
 
-acc = history.history['accuracy']
-val_acc = history.history['val_accuracy']
+# acc = history.history['accuracy']
+# val_acc = history.history['val_accuracy']
 
-plt.plot(epochs, acc, 'y', label='Training accuracy')
-plt.plot(epochs, val_acc, 'r', label='Validation accuracy')
-plt.title('Training and validation accuracy')
-plt.xlabel('Epochs')
-plt.ylabel('Accuracy')
-plt.legend()
-plt.show()
-#################################################
-from keras.models import load_model
+# plt.plot(epochs, acc, 'y', label='Training accuracy')
+# plt.plot(epochs, val_acc, 'r', label='Validation accuracy')
+# plt.title('Training and validation accuracy')
+# plt.xlabel('Epochs')
+# plt.ylabel('Accuracy')
+# plt.legend()
+# plt.show()
+# #################################################
+# from keras.models import load_model
 
-# Load model for prediction or continue training
+# # Load model for prediction or continue training
 
-# For continuing training....
-# The following gives an error: Unknown loss function: dice_loss_plus_1focal_loss
-# This is because the model does not save loss function and metrics. So to compile and
-# continue training we need to provide these as custom_objects.
-my_model = load_model('brats_3d.hdf5')
+# # For continuing training....
+# # The following gives an error: Unknown loss function: dice_loss_plus_1focal_loss
+# # This is because the model does not save loss function and metrics. So to compile and
+# # continue training we need to provide these as custom_objects.
+# my_model = load_model('brats_3d.hdf5')
 
-# So let us add the loss as custom object... but the following throws another error...
-# Unknown metric function: iou_score
-my_model = load_model('brats_3d.hdf5',
-                      custom_objects={'dice_loss_plus_1focal_loss': total_loss})
+# # So let us add the loss as custom object... but the following throws another error...
+# # Unknown metric function: iou_score
+# my_model = load_model('brats_3d.hdf5',
+#                       custom_objects={'dice_loss_plus_1focal_loss': total_loss})
 
-# Now, let us add the iou_score function we used during our initial training
-my_model = load_model('brats_3d.hdf5',
-                      custom_objects={'dice_loss_plus_1focal_loss': total_loss,
-                                      'iou_score': sm.metrics.IOUScore(threshold=0.5)})
+# # Now, let us add the iou_score function we used during our initial training
+# my_model = load_model('brats_3d.hdf5',
+#                       custom_objects={'dice_loss_plus_1focal_loss': total_loss,
+#                                       'iou_score': sm.metrics.IOUScore(threshold=0.5)})
 
-# Now all set to continue the training process.
-history2 = my_model.fit(train_img_datagen,
-                        steps_per_epoch=steps_per_epoch,
-                        epochs=1,
-                        verbose=1,
-                        validation_data=val_img_datagen,
-                        validation_steps=val_steps_per_epoch,
-                        )
-#################################################
+# # Now all set to continue the training process.
+# history2 = my_model.fit(train_img_datagen,
+#                         steps_per_epoch=steps_per_epoch,
+#                         epochs=1,
+#                         verbose=1,
+#                         validation_data=val_img_datagen,
+#                         validation_steps=val_steps_per_epoch,
+#                         )
+# #################################################
 
-# For predictions you do not need to compile the model, so ...
-my_model = load_model('brats_3d.hdf5',
-                      compile=False)
+# # For predictions you do not need to compile the model, so ...
+# my_model = load_model('brats_3d.hdf5',
+#                       compile=False)
 
-# Verify IoU on a batch of images from the test dataset
-# Using built in keras function for IoU
-# Only works on TF > 2.0
-from keras.metrics import MeanIoU
+# # Verify IoU on a batch of images from the test dataset
+# # Using built in keras function for IoU
+# # Only works on TF > 2.0
+# from keras.metrics import MeanIoU
 
-batch_size = 4  # Check IoU for a batch of images
-test_img_datagen = imageLoader(val_img_dir, val_img_list,
-                               val_mask_dir, val_mask_list, batch_size)
+# batch_size = 4  # Check IoU for a batch of images
+# test_img_datagen = imageLoader(val_img_dir, val_img_list,
+#                                val_mask_dir, val_mask_list, batch_size)
 
-# Verify generator.... In python 3 next() is renamed as __next__()
-test_image_batch, test_mask_batch = test_img_datagen.__next__()
+# # Verify generator.... In python 3 next() is renamed as __next__()
+# test_image_batch, test_mask_batch = test_img_datagen.__next__()
 
-test_mask_batch_argmax = np.argmax(test_mask_batch, axis=4)
-test_pred_batch = my_model.predict(test_image_batch)
-test_pred_batch_argmax = np.argmax(test_pred_batch, axis=4)
+# test_mask_batch_argmax = np.argmax(test_mask_batch, axis=4)
+# test_pred_batch = my_model.predict(test_image_batch)
+# test_pred_batch_argmax = np.argmax(test_pred_batch, axis=4)
 
-#val_loss, val_dice = model.evaluate(val_img_datagen)
-#print(f"validation soft dice loss: {val_loss:.4f}")
-#print(f"validation dice coefficient: {val_dice:.4f}")
+# #val_loss, val_dice = model.evaluate(val_img_datagen)
+# #print(f"validation soft dice loss: {val_loss:.4f}")
+# #print(f"validation dice coefficient: {val_dice:.4f}")
 
-#n_classes = 4
-#IOU_keras = MeanIoU(num_classes=n_classes)
-#IOU_keras.update_state(test_pred_batch_argmax, test_mask_batch_argmax)
-#print("Mean IoU =", IOU_keras.result().numpy())
+# #n_classes = 4
+# #IOU_keras = MeanIoU(num_classes=n_classes)
+# #IOU_keras.update_state(test_pred_batch_argmax, test_mask_batch_argmax)
+# #print("Mean IoU =", IOU_keras.result().numpy())
 
-#brain val
-val_num = 12
-test_val = np.load("C:\\Users\\samue\\BraTS2020\\BraTS2020_TrainingData\\input_data_3channels\\images\\image_" + str(val_num) + ".npy")
+# #brain val
+# val_num = 12
+# test_val = np.load("C:\\Users\\samue\\BraTS2020\\BraTS2020_TrainingData\\input_data_3channels\\images\\image_" + str(val_num) + ".npy")
 
-test_val_img_input = np.expand_dims(test_val, axis=0)
-test_val_prediction = my_model.predict(test_val_img_input)
-test_val_prediction_argmax = np.argmax(test_val_prediction, axis=4)[0, :, :, :]
+# test_val_img_input = np.expand_dims(test_val, axis=0)
+# test_val_prediction = my_model.predict(test_val_img_input)
+# test_val_prediction_argmax = np.argmax(test_val_prediction, axis=4)[0, :, :, :]
 
-n_slice_val=70
+# n_slice_val=70
 
-plt.figure(figsize=(12, 8))
-plt.subplot(231)
-plt.title('Validation Test Image')
-plt.imshow(test_val[:, :, n_slice_val, 1], cmap='gray')
-plt.subplot(232)
-plt.title('Prediction on validation image')
-plt.imshow(test_val_prediction_argmax[:, :, n_slice_val])
-plt.show()
+# plt.figure(figsize=(12, 8))
+# plt.subplot(231)
+# plt.title('Validation Test Image')
+# plt.imshow(test_val[:, :, n_slice_val, 1], cmap='gray')
+# plt.subplot(232)
+# plt.title('Prediction on validation image')
+# plt.imshow(test_val_prediction_argmax[:, :, n_slice_val])
+# plt.show()
 
 
-#liver TEST val###############################
+# #liver TEST val###############################
 
-test_liver = np.load("D:\\MDS\\dataset\\Task03_Liver\\numpy\\val\\liverTEST_3.npy")
-liver_mask = np.load("D:\\MDS\\dataset\\Task03_Liver\\numpy\\val\\liverTEST_3.npy")
+# test_liver = np.load("D:\\MDS\\dataset\\Task03_Liver\\numpy\\val\\liverTEST_3.npy")
+# liver_mask = np.load("D:\\MDS\\dataset\\Task03_Liver\\numpy\\val\\liverTEST_3.npy")
 
-test_val_liver_input = np.expand_dims(test_liver, axis=0)
-test_val_liver_prediction = my_model.predict(test_val_liver_input)
-test_val_liver_prediction_argmax = np.argmax(test_val_liver_prediction, axis=4)[0, :, :, :]
-test_mask_argmax = np.argmax(test_mask, axis=1)
+# test_val_liver_input = np.expand_dims(test_liver, axis=0)
+# test_val_liver_prediction = my_model.predict(test_val_liver_input)
+# test_val_liver_prediction_argmax = np.argmax(test_val_liver_prediction, axis=4)[0, :, :, :]
+# test_mask_argmax = np.argmax(test_mask, axis=1)
 
-n_slice_liver=random.randint(90,128)
+# n_slice_liver=random.randint(90,128)
 
-plt.figure(figsize=(12, 8))
-plt.subplot(231)
-plt.title('Testing Image')
-plt.imshow(test_liver[:, :, n_slice_liver, 1], cmap='gray')
-plt.subplot(232)
-plt.title('Prediction on test image')
-plt.imshow(test_val_liver_prediction_argmax[:, :, n_slice_liver])
-plt.show()
+# plt.figure(figsize=(12, 8))
+# plt.subplot(231)
+# plt.title('Testing Image')
+# plt.imshow(test_liver[:, :, n_slice_liver, 1], cmap='gray')
+# plt.subplot(232)
+# plt.title('Prediction on test image')
+# plt.imshow(test_val_liver_prediction_argmax[:, :, n_slice_liver])
+# plt.show()
